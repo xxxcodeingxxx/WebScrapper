78b4eb3009596b3eb62162107681cc08775ade2d
xxxcodeingxxx <downspropertym@gmail.com>
2023-03-05 22:42:10 -0500

    minor changes

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const fs = require("fs");
 const puppeteer = require("puppeteer");
-const pageToScrape = "https://traversymedia.com";
+const pageToScrape = "https://chninc.org/about-chn/partners-supporters.html";
 
 async function run() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--use-gl=egl"],
+  });
   const page = await browser.newPage();
   await page.goto(pageToScrape);
-  await page.waitForSelector("#cscourses .card");
+  await page.waitForSelector(".primary_content");
   //   await page.setViewport({ width: 1920, height: 1080 });
 
   //   await page.screenshot({ path: "example.png", fullPage: true });
@@ -14,17 +17,25 @@ async function run() {
 
   //   const html = await page.content();
 
-  const courses = await page.$$eval("#cscourses .card", (elements) =>
+  //   const courses = await page.$$eval("#cscourses .card", (elements) =>
+  //     elements.map((e) => ({
+  //       title: e.querySelector(".card-body h3").innerText,
+  //       level: e.querySelector(".card-body .level").innerText,
+  //       url: e.querySelector(".card-footer a").href,
+  //       //   promo: e.querySelector(".card-footer .promo-code .promo").innerText,
+  //     }))
+  //   );
+
+  const chn = await page.$$eval(".primary_content", (elements) =>
     elements.map((e) => ({
-      title: e.querySelector(".card-body h3").innerText,
-      level: e.querySelector(".card-body .level").innerText,
-      url: e.querySelector(".card-footer a").href,
-      //   promo: e.querySelector(".card-footer .promo-code .promo").innerText,
+      first: e.querySelector("h1").innerText,
+      paraOne: e.querySelector("p").innerText,
+      title: e.querySelector("h3").innerText,
     }))
   );
-  //   console.log(courses);
+
   // Save data to JSON file.....
-  fs.writeFile("courses.json", JSON.stringify(courses), (err) => {
+  fs.writeFile("chn.json", JSON.stringify(chn), (err) => {
     if (err) throw err;
     console.log("File saved....");
   });
