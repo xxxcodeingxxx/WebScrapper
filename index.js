b76f9a4a3de6042783a6980a8618ee5f96a3e228
xxxcodeingxxx <downspropertym@gmail.com>
2023-03-10 21:04:58 -0500

    minor update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const puppeteer = require("puppeteer");
-const pageToScrape = "https://chninc.org/about-chn/partners-supporters.html";
+const pageToScrape = "https://chninc.org/about-chn/our-purpose.html";
 
 async function run() {
   const browser = await puppeteer.launch({
@@ -9,13 +9,13 @@ async function run() {
   });
   const page = await browser.newPage();
   await page.goto(pageToScrape);
-  //   await page.waitForSelector(".primary_content");
-  await page.setViewport({ width: 1920, height: 1080 });
+  await page.waitForSelector("html");
+  // await page.setViewport({ width: 1920, height: 1080 });
 
-  await page.screenshot({ path: "chn.png", fullPage: true });
+  // await page.screenshot({ path: "chn.png", fullPage: true });
   //   await page.pdf({ path: "chn.pdf", format: "A4" });
 
-  //   const html = await page.content();
+  const html = await page.content();
 
   //   const courses = await page.$$eval("#cscourses .card", (elements) =>
   //     elements.map((e) => ({
@@ -26,19 +26,22 @@ async function run() {
   //     }))
   //   );
 
-  //   const chn = await page.$$eval(".primary_content", (elements) =>
+  // const chn = await page.$$eval(
+  //   ".collection-item .collection-item__content",
+  //   (elements) =>
   //     elements.map((e) => ({
-  //       first: e.querySelector("h1").innerText,
-  //       paraOne: e.querySelector("p").innerText,
-  //       title: e.querySelector("h3").innerText,
+  //       title: e.querySelector(".collection-item-label").innerText,
+  //       description: e.querySelector(".collection-item-description p")
+  //         .innerText,
   //     }))
-  //   );
+  // );
 
   // Save data to JSON file.....
-  //   fs.writeFile("index.html", html, (err) => {
-  //     if (err) throw err;
-  //     console.log("File saved....");
-  //   });
+  fs.writeFile("purpose.html", html, (err) => {
+    if (err) throw err;
+    console.log("File saved....");
+  });
+
   await browser.close();
 }
 
